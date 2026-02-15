eb21cd99e8f8be8bcf05516b0e5ba1d0ceaf0b61
Anupam Rana <141600262+anupamrana200@users.noreply.github.com>
2026-02-16 01:52:44 +0530

    Added the observability, i.e - langsmith added into chatbot project.

--- chatbot/frontendDatabase.py
+++ chatbot/frontendDatabase.py
@@ -80,7 +80,10 @@ if user_input:
     st.text(user_input)
 
   
-  Config = {'configurable': {'thread_id':st.session_state['thread_id']}}
+  Config = {'configurable': {'thread_id':st.session_state['thread_id']},
+            'metadata':{'thread_id':st.session_state['thread_id']},
+            'run_name':'chat_turn'
+           }
 
   with st.chat_message('assistant'):
     ai_message = st.write_stream(
